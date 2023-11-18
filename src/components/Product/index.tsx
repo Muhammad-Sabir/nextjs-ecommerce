177b035faa82750fac5eeff237377232f5984cdf
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-11-18 21:52:21 +0500

    Delete card component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -14,13 +14,13 @@ export default function Product({ product }: { product: Product }) {
   return (
     <Link
       href={`/product/${product._id}`}
-      className="border-cardWhite flex flex-col items-center rounded-md border-4 bg-transparent bg-clip-border p-4 sm:items-stretch"
+      className="flex flex-col items-center rounded-md border-4 border-cardWhite bg-transparent bg-clip-border p-4 sm:items-stretch"
     >
       <Image src={product.image} alt={product.name} width={350} height={350} />
 
       <div className="pt-4 text-left">
         <p className="text-sm text-gray-400">{product.category}</p>
-        <h3 className="text-blackishBlue max-w-xs overflow-hidden overflow-ellipsis whitespace-nowrap py-2 font-bold">
+        <h3 className="max-w-xs overflow-hidden overflow-ellipsis whitespace-nowrap py-2 font-bold text-blackishBlue">
           {product.name}
         </h3>
         <p className="text-sm font-bold text-blue-500">{product.price}</p>
